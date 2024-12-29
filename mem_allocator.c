c80e9e0545705121da8e1db9c62d4567915947af
root <root@DESKTOP-V7IA394.>
2024-12-28 22:18:06 -0500

    wrote out logic for coalescing

--- mem_allocator.c
+++ mem_allocator.c
@@ -10,6 +10,7 @@ typedef struct block_header {
     void * prev; // previous data ptr in contiguous memory
     char alloc; // 0 for unallocated 1 for allocated
     char end; // 1 if block is last in page else 0
+    char rm; // 1 if block can be removed from free list (after coalescing) else 0
 } block_header;
 
 #define OVERHEAD sizeof(block_header)
@@ -31,21 +32,24 @@ static inline void * get_next(void * ptr) {
     return ((char *)(ptr) + get_header(ptr)->size + OVERHEAD);
 }
 
+// pass in block header pointer to be added to free list
 void append_free_list(block_header * bh) {
     bh->next = free_list_head;
     free_list_head = bh;
 }
 
-void remove_free_list(block_header * bh) {
+block_header * remove_free_list(block_header * bh) {
     if (bh == free_list_head) {
         free_list_head = free_list_head->next;
-        return;
+        block_header * tmp;
+        tmp->next = free_list_head;
+        return tmp;
     }
     block_header * traverse = free_list_head;
     while (traverse->next) {
         if (traverse->next == bh) {
             traverse->next == traverse->next->next;
-            return;
+            return traverse;
         }
     }
 }
@@ -59,7 +63,9 @@ void * alloc_mem(size_t bytes) {
 
     while (traverse) {
         // traverse free list and look for unallocated block that is large enough
-        if (traverse->size >= bytes) {
+        if (traverse->rm) {
+            remove_free_list(traverse);
+        } else if (traverse->size >= bytes) {
             ptr = traverse;
             remove_free_list(traverse);
             break;
@@ -83,6 +89,7 @@ void * alloc_mem(size_t bytes) {
     ((block_header *)ptr)->alloc = 1;
     ((block_header *)ptr)->size = bytes;
     ((block_header *)ptr)->end = 0;
+    ((block_header *)ptr)->rm = 0;
 
     // increment ptr to actual data
     ptr = ((char *)(ptr) + OVERHEAD);
@@ -103,38 +110,52 @@ void * alloc_mem(size_t bytes) {
 }
 
 void free_mem(void * ptr) {
-    // append_free_list(get_header(ptr));
     block_header * header = get_header(ptr);
+    block_header * prev_block;
+    block_header * next_block;
     char prev_alloc = 1;
     char next_alloc = 1;
 
     if (header->prev) {
-        block_header * prev_block = get_header(header->prev);
+        prev_block = get_header(header->prev);
         prev_alloc = prev_block->alloc;
     }
 
     if (!header->end) {
-        block_header * next_block = (char *)header + OVERHEAD + header->size;
+        next_block = (block_header *)((char *)header + OVERHEAD + header->size);
         next_alloc = next_block->alloc;
     }
 
     // coalescing
     if (!prev_alloc && !next_alloc) {
+        // total size = prev + current + next + 2 * OVERHEAD
+        prev_block->size += header->size + next_block->size + 2 * OVERHEAD;
+        header->rm = 1;
+        next_block->rm = 1;
 
+        // don't need to add to free list since prev is already in free list
     } else if (!prev_alloc && next_alloc) {
+        prev_block->size += header->size + OVERHEAD;
 
+        // no need to set rm for header to 1 since header is not in free list
+        // don't need to add to free list since prev is already in free list
     } else if (prev_alloc && !next_alloc) {
+        header->size += next_block->size + OVERHEAD;
+        next_block->rm = 1;
 
+        append_free_list(header);
     } else {
-        
+        append_free_list(header);
     }
 }
 
 int main() {
-    void * test = alloc_mem(4096 - OVERHEAD);
+    void * test = alloc_mem(32);
     block_header * header = get_header(test);
-    // free_mem(test);
-    void * test2 = alloc_mem(10);
+    void * test2 = alloc_mem(32);
     block_header * header2 = get_header(test2);
+    void * test3 = alloc_mem(32);
+    block_header * header3 = get_header(test3);
+    free_mem(test2);
     return 0;
 }
